f847f289fa7747ff86f9c82c4d558c4dba1d9b33
Avhale <sumitavhale310@gmail.com>
2026-05-28 16:18:02 +0530

    controllers for tweet, comment, like and subscription then routes for each

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,10 +1,25 @@
 import {Router} from "express"
 import { verifyJwt } from "../middlewares/auth.middleware";
-import {toggleVideoLike} from "../controllers/like.controller.js"
+import {toggleVideoLike,toggleCommentLike,toggleTweetLike, getLikedVideos} from "../controllers/like.controller.js"
 
 const router = Router();
 
 router.route("/toggle/v/:videoId").post(
     verifyJwt,
     toggleVideoLike
+)
+
+router.route("/toggle/c/:commentId").post(
+    verifyJwt,
+    toggleCommentLike
+)
+
+router.route("toggle/t/:tweetId").post(
+    verifyJwt,
+    toggleTweetLike
+)
+
+router.route("/videos").get(
+    verifyJwt,
+    getLikedVideos
 )
